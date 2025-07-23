fc1db4453d58b7104bae1d8e81d56f83637e0675
maryam-zia <maryam.zia@unity3d.com>
2025-07-23 16:40:46 -0400

    Update assembly

--- com.unity.ml-agents/Runtime/AssemblyInfo.cs
+++ com.unity.ml-agents/Runtime/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Unity.ML-Agents.Runtime.Sensor.Tests")]
 [assembly: InternalsVisibleTo("Unity.ML-Agents.Runtime.Utils.Tests")]
 [assembly: InternalsVisibleTo("Unity.ML-Agents.Runtime.Tests")]
+[assembly: InternalsVisibleTo("Unity.ML-Agents.Runtime.Input.Tests")]
 [assembly: InternalsVisibleTo("Unity.ML-Agents.Editor")]
 [assembly: InternalsVisibleTo("Unity.ML-Agents.Pro")]
 [assembly: InternalsVisibleTo("Unity.ML-Agents.Pro.Tests")]
