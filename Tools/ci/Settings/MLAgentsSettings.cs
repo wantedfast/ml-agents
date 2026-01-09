09abb37b940ba39a0db7bc18e7d9eafceca86327
Bob Donovan <bobdonov@gmail.com>
2026-01-09 12:18:10 -0500

    Adding the Wrench Project and Recipe Engine
    Updating the Wrench jobs

---
+++ Tools/ci/Settings/MLAgentsSettings.cs
@@ -0,0 +1,30 @@
+﻿using RecipeEngine.Api.Settings;
+using RecipeEngine.Modules.Wrench.Models;
+using RecipeEngine.Modules.Wrench.Settings;
+
+namespace MLAgents.Cookbook.Settings;
+
+public class MLAgentsSettings : AnnotatedSettingsBase
+{
+    // Path from the root of the repository where packages are located.
+    readonly string[] PackagesRootPaths = {"."};
+
+    // update this to list all packages in this repo that you want to release.
+    Dictionary<string, PackageOptions> PackageOptions = new()
+    {
+        {
+            "com.unity.ml-agents",
+            new PackageOptions() { ReleaseOptions = new ReleaseOptions() { IsReleasing = true } }
+        }
+    };
+
+    public MLAgentsSettings()
+    {
+        Wrench = new WrenchSettings(
+            PackagesRootPaths,
+            PackageOptions
+        );      
+    }
+
+    public WrenchSettings Wrench { get; private set; }
+}
